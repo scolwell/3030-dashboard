c55cfa3fb897e6499e01f6f813a57aa215a2ce47
Scott Colwell <scolwell@uoguelph.ca>
2026-02-07 17:03:58 -0500

    Fix story demo base path

--- components/HypothesisTestStory.tsx
+++ components/HypothesisTestStory.tsx
@@ -4,7 +4,7 @@ const HypothesisTestStory: React.FC = () => {
   return (
     <div className="h-full w-full">
       <iframe
-        src="/demos/story-of-uncertainty/index.html"
+        src={`${import.meta.env.BASE_URL}demos/story-of-uncertainty/index.html`}
         className="w-full h-full border-none rounded-3xl"
         title="Story of Uncertainty"
         allow="fullscreen"
